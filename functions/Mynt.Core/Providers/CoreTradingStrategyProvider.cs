02d89bfaf81d27208232cc44e8f7dae48b3417a6
Jeroen <j.g.snijders@gmail.com>
2018-01-24 00:15:17 +0100

    - Added a WPF project to set up an alternative for the console application; - Added a plugin loader so trading strategies can be added without having to reference them; - Fixed a version problem with System.Net.Http.

---
+++ functions/Mynt.Core/Providers/CoreTradingStrategyProvider.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Mynt.Core.Interfaces;
+using Mynt.Core.Strategies;
+
+namespace Mynt.Core.Providers
+{
+    class CoreTradingStrategyProvider : ITradingStrategyProvider
+    {
+        public IEnumerable<ITradingStrategy> CreateStrategies()
+        {
+            return new List<ITradingStrategy>()
+            {
+                new AdxMomentum(),
+                new AdxSmas(),
+                new AwesomeMacd(),
+                new AwesomeSma(),
+                new Base150(),
+                new BbandRsi(),
+                new BigThree(),
+                new BreakoutMa(),
+                new BuyAndHold(),
+                new CciEma(),
+                new CciRsi(),
+                new CciScalper(),
+                new DerivativeOscillator(),
+                new DoubleVolatility(),
+                new EmaAdx(),
+                new EmaAdxF(),
+                new EmaAdxMacd(),
+                new EmaAdxSmall(),
+                new EmaCross(),
+                new EmaStochRsi(),
+                new FaMaMaMa(),
+                new FifthElement(),
+                new Fractals(),
+                new FreqTrade(),
+                new MacdSma(),
+                new MacdTema(),
+                new Momentum(),
+                new PowerRanger(),
+                new RsiBbands(),
+                new RsiMacd(),
+                new RsiMacdAwesome(),
+                new RsiMacdMfi(),
+                new RsiSarAwesome(),
+                new SarAwesome(),
+                new SarRsi(),
+                new SarStoch(),
+                new SimpleBearBull(),
+                new SmaCrossover(),
+                new SmaSar(),
+                new SmaStochRsi(),
+                new StochAdx(),
+                new ThreeMAgos(),
+                new TripleMa(),
+                new Wvf(),
+                new WvfExtended()
+            };
+        }
+    }
+}
---
+++ functions/Mynt.PluginLoader/PluginLoader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Mynt.Extensibility
+{
+    public static class PluginLoader
+    {
+        public static IEnumerable<T> Create<T>() where T : class
+        {
+            var assemblyNames = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.dll");
+
+            List<T> results = new List<T>();
+            foreach (var assemblyName in assemblyNames)
+            {
+                try
+                {
+                    Assembly assembly = Assembly.LoadFile(assemblyName);
+                    var types = assembly.GetTypes().Where(_ => _.IsInstanceOfType(typeof(T)) ||
+                        _.GetInterfaces().Contains(typeof(T))).Where(_ => _.GetConstructor(Type.EmptyTypes) != null);
+                    results.AddRange(types.Select(_ => (T)Activator.CreateInstance(_)));
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+            return results;
+        }
+    }
+}
