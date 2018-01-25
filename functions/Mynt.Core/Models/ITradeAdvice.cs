7d17de5880b80ae06786698fcafa0e01ba5c4ff4
Jeroen <j.g.snijders@gmail.com>
2018-01-25 07:30:19 +0100

    Added a ITradeAdvice interface with two implementations.

---
+++ functions/Mynt.Core/Models/ITradeAdvice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mynt.Core.Enums;
+
+namespace Mynt.Core.Models
+{
+    public interface ITradeAdvice
+    {
+        TradeAdvice TradeAdvice { get; }
+    }
+}
---
+++ functions/Mynt.Core/Models/SimpleTradeAdvice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mynt.Core.Enums;
+
+namespace Mynt.Core.Models
+{
+    public class SimpleTradeAdvice :ITradeAdvice
+    {
+        private readonly TradeAdvice tradeAdvice;
+
+        public SimpleTradeAdvice(TradeAdvice tradeAdvice)
+        {
+            this.tradeAdvice = tradeAdvice;
+        }
+
+        public TradeAdvice TradeAdvice => tradeAdvice;
+
+        public override string ToString()
+        {
+            return $"TradeAdvice: {tradeAdvice.ToString()}";
+        }
+    }
+}
