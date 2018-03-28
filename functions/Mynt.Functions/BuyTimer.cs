e5af3c392a4b547c12bc764dac09b402b3c9dbf3
Steven Thewissen <github@thewissen.io>
2018-03-28 23:01:18 +0200

    Added the functions timers back in

---
+++ functions/Mynt.Functions/BuyTimer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace Mynt.Functions
+{
+    public static class BuyTimer
+    {
+        [FunctionName("BuyTimer")]
+        public static async Task Run([TimerTrigger("10 1 * * * *")]TimerInfo buyTimer, TraceWriter log)
+        {
+            try
+            {
+                log.Info("Starting processing...");
+
+
+
+                log.Info("Done...");
+            }
+            catch (Exception ex)
+            {
+                // If anything goes wrong log an error to Azure.
+                log.Error(ex.Message + ex.StackTrace);
+            }
+        }
+    }
+}
+}
---
+++ functions/Mynt.Functions/SellTimer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace Mynt.Functions
+{
+    public static class SellTimer
+    {
+        [FunctionName("SellTimer")]
+        public static async Task Run([TimerTrigger("0 * * * * *")]TimerInfo sellTimer, TraceWriter log)
+        {
+            try
+            {
+                log.Info("Starting processing...");
+
+               
+
+                log.Info("Done...");
+            }
+            catch (Exception ex)
+            {
+                // If anything goes wrong log an error to Azure.
+                log.Error(ex.Message + ex.StackTrace);
+            }
+        }
+    }
+}
