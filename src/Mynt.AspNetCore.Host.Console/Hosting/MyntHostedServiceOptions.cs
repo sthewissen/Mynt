aac0237b48e03633deddbaedab4fe1aa2deffa9b
Steven Thewissen <github@thewissen.io>
2018-04-11 18:53:33 +0200

    Added custom binding redirect option

--- src/Mynt.AspNetCore.Host.Console/Hosting/MyntHostedServiceOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mynt.AspNetCore.Host.Hosting
-{
-    public class MyntHostedServiceOptions
-    {
-        public string BuyTimer { get; set; }
-        public string SellTimer { get; set; }
-    }
-}
