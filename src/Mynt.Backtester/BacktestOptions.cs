fc127d38fdb107216884b24d4479f3bd3f729ad7
Steven <github@thewissen.io>
2018-04-30 23:42:03 +0200

    Minor strategt fixes

--- src/Mynt.Backtester/BacktestOptions.cs
+++ src/Mynt.Backtester/BacktestOptions.cs
@@ -6,7 +6,7 @@ namespace Mynt.Backtester
     public class BacktestOptions
     {
         public decimal StakeAmount { get; set; } = 0.1m;
-        public bool OnlyStartNewTradesWhenSold { get; set; } = false;
+        public bool OnlyStartNewTradesWhenSold { get; set; } = true;
         public List<string> Coins { get; set; } = new List<string> { };
     }
 }
