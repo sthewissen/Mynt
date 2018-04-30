2429c6c964542754c7a74ad892abd1b32c3b507e
Steven <github@thewissen.io>
2018-04-30 21:14:18 +0200

    Minor strategy tweaks

--- src/Mynt.Core/Strategies/CciScalper.cs
+++ src/Mynt.Core/Strategies/CciScalper.cs
@@ -14,26 +14,23 @@ namespace Mynt.Core.Strategies
     public class CciScalper : BaseStrategy
     {
         public override string Name => "CCI Scalper";
-        public override int MinimumAmountOfCandles => 200;
+        public override int MinimumAmountOfCandles => 14;
         public override Period IdealPeriod => Period.Hour;
 
         public override List<TradeAdvice> Prepare(List<Candle> candles)
         {
             var result = new List<TradeAdvice>();
 
-            if (candles.Count < 200)
-                throw new Exception("Need larger data set: (200 min).");
-
-            var cci = candles.Cci(200);
+            var cci = candles.Cci();
             var ema10 = candles.Ema(10);
             var ema21 = candles.Ema(21);
             var ema50 = candles.Ema(50);
 
             for (int i = 0; i < candles.Count; i++)
             {
-                if (cci[i] > 0 && ema10[i] > ema21[i] && ema10[i] > ema50[i])
+                if (cci[i] < -100 && ema10[i] > ema21[i] && ema10[i] > ema50[i])
                     result.Add(TradeAdvice.Buy);
-                else if (cci[i] < 0 && ema10[i] < ema21[i] && ema10[i] < ema50[i])
+                else if (cci[i] > 100 && ema10[i] < ema21[i] && ema10[i] < ema50[i])
                     result.Add(TradeAdvice.Sell);
                 else
                     result.Add(TradeAdvice.Hold);
