12e8162a0dab13e4f935912db6208de2902f765e
Steven <github@thewissen.io>
2018-05-13 10:52:59 +0200

    Added experimental support resistance finder
    This may be something people can use in their strategies at some point in the future. Currently it's still a separate console project which poops out a TradingView script to see the results. Should be going into Core at some point.

---
+++ src/Mynt.ResistanceSupport/Models/PivotPoint.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Mynt.ResistanceSupport.Models
+{
+    public class PivotPoint
+    {
+		public DateTime Timestamp { get; set; }
+		public decimal Price { get; set; }
+		public PivotType Type { get; set; }
+    }
+}
