0486607276715be9ab5a1a0be3dd4a23843ca19b
LORDofDOOM <tb@bbz-int.de>
2018-05-14 07:13:06 +0200

    Add MongoDB DataStore (without EF dependency)

---
+++ src/Mynt.Data.MongoDB/TradeAdapter.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Mynt.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mynt.Data.MongoDB
+{
+    public class TradeAdapter
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        public ObjectId Id { get; set; }
+        public string TradeId { get; set; }
+        public string TraderId { get; set; }
+        public string Market { get; set; }
+
+        public double OpenRate { get; set; }
+        public double? CloseRate { get; set; }
+        public double? CloseProfit { get; set; }
+        public double? CloseProfitPercentage { get; set; }
+
+        public double StakeAmount { get; set; }
+        public double Quantity { get; set; }
+
+        public bool IsOpen { get; set; }
+        public bool IsBuying { get; set; }
+        public bool IsSelling { get; set; }
+
+        public string OpenOrderId { get; set; }
+        public string BuyOrderId { get; set; }
+        public string SellOrderId { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime OpenDate { get; set; }
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime? CloseDate { get; set; }
+
+        public string StrategyUsed { get; set; }
+        public double? StopLossRate { get; set; }
+        public SellType SellType { get; set; }
+    }
+}
