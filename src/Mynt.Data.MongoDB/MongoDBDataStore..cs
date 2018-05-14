81feb60530b38f6e97f4ca1b6c2e288eed0cb034
LORDofDOOM <tb@bbz-int.de>
2018-05-14 12:27:59 +0200

    Add MongoDB DataStore

---
+++ src/Mynt.Data.MongoDB/MongoDBDataStore..cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using Mynt.Core.Interfaces;
+using Mynt.Core.Models;
+
+namespace Mynt.Data.MongoDB
+{
+    public class MongoDBDataStore : IDataStore
+    {
+        private MongoClient client;
+        private IMongoDatabase database;
+        private IMongoCollection<TraderAdapter> traderAdapter;
+        private IMongoCollection<TradeAdapter> ordersAdapter;
+
+        //private readonly MyntDbContext _context;
+
+        public MongoDBDataStore()
+        {
+            //_context = new MyntDbContext(options.SqliteConnectionString);
+            client = new MongoClient("mongodb://127.0.0.1:27017");
+            database = client.GetDatabase("Mynt");
+            ordersAdapter = database.GetCollection<TradeAdapter>("Orders");
+            traderAdapter = database.GetCollection<TraderAdapter>("Trader");
+        }
+
+        public async Task InitializeAsync()
+        {
+        }
+
+        public async Task<List<Trade>> GetActiveTradesAsync()
+        {
+            var trades = await ordersAdapter.Find(x => x.IsOpen).ToListAsync();
+            var items = Mapping.Mapper.Map<List<Trade>>(trades);
+
+            return items;
+        }
+
+        public async Task<List<Trader>> GetAvailableTradersAsync()
+        {
+            var traders = await traderAdapter.Find(x => !x.IsBusy && !x.IsArchived).ToListAsync();
+            var items = Mapping.Mapper.Map<List<Trader>>(traders);
+
+            return items;
+        }
+
+        public async Task<List<Trader>> GetBusyTradersAsync()
+        {
+            var traders = await traderAdapter.Find(x => x.IsBusy && !x.IsArchived).ToListAsync();
+            var items = Mapping.Mapper.Map<List<Trader>>(traders);
+
+            return items;
+        }
+
+        public async Task SaveTradeAsync(Trade trade)
+        {
+            var item = Mapping.Mapper.Map<TradeAdapter>(trade);
+            TradeAdapter checkExist = await ordersAdapter.Find(x => x.TradeId.Equals(item.TradeId)).FirstOrDefaultAsync();
+            if (checkExist != null)
+            {
+                await ordersAdapter.ReplaceOneAsync(x => x.TradeId.Equals(item.TradeId), item);
+            } else
+            {
+                await ordersAdapter.InsertOneAsync(item);
+            }
+        }
+
+        public async Task SaveTraderAsync(Trader trader)
+        {
+            var item = Mapping.Mapper.Map<TraderAdapter>(trader);
+            TraderAdapter checkExist = await traderAdapter.Find(x => x.Identifier.Equals(item.Identifier)).FirstOrDefaultAsync();
+            if (checkExist != null)
+            {
+                await traderAdapter.ReplaceOneAsync(x => x.Identifier.Equals(item.Identifier), item);
+            }
+            else
+            {
+                await traderAdapter.InsertOneAsync(item);
+            }
+        }
+
+        public async Task SaveTradersAsync(List<Trader> traders)
+        {
+            var items = Mapping.Mapper.Map<List<TraderAdapter>>(traders);
+
+            foreach (var item in items)
+            {
+                TraderAdapter checkExist = await traderAdapter.Find(x => x.Identifier.Equals(item.Identifier)).FirstOrDefaultAsync();
+                if (checkExist != null)
+                {
+                    await traderAdapter.ReplaceOneAsync(x => x.Identifier.Equals(item.Identifier), item);
+                }
+                else
+                {
+                    await traderAdapter.InsertOneAsync(item);
+                }
+            }
+        }
+
+        public async Task SaveTradesAsync(List<Trade> trades)
+        {
+            var items = Mapping.Mapper.Map<List<TradeAdapter>>(trades);
+
+            foreach (var item in items)
+            {
+                TradeAdapter checkExist = await ordersAdapter.Find(x => x.TradeId.Equals(item.TradeId)).FirstOrDefaultAsync();
+                if (checkExist != null)
+                {
+                    await ordersAdapter.ReplaceOneAsync(x => x.TradeId.Equals(item.TradeId), item);
+                }
+                else
+                {
+                    await ordersAdapter.InsertOneAsync(item);
+                }
+            }
+        }
+
+        public async Task<List<Trader>> GetTradersAsync()
+        {
+            var traders = await traderAdapter.Find(_ => true).ToListAsync();
+            var items = Mapping.Mapper.Map<List<Trader>>(traders);
+
+            return items;
+        }
+    }
+}
