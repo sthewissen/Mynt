5ec0eabb35a2e8a0cc5510c495c8903f079294d0
Steven <github@thewissen.io>
2018-05-29 15:40:54 +0200

    Added PivotHigh and PivotLow indicators

---
+++ src/Mynt.Core/Indicators/PivotLow.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Mynt.Core.Models;
+using System.Linq;
+
+namespace Mynt.Core.Indicators
+{
+
+	public static partial class Extensions
+	{
+		public static List<decimal?> PivotLow(this List<Candle> source, int barsLeft = 4, int barsRight = 2, bool fillNullValues = false)
+		{
+			var result = new List<decimal?>();
+            
+			for (int i = 0; i < source.Count; i++)
+			{
+				if (i < barsLeft + barsRight)
+				{
+					result.Add(null);
+					continue;
+				}
+                
+				var isPivot = true;
+				var subSet = source.Skip(i - barsLeft - barsRight).Take(barsLeft + barsRight + 1).ToList();
+				var valueToCheck = subSet[barsLeft];
+
+				// Check if the [barsLeft] bars left of what we're checking all have lower highs or equal
+				for (int leftPivot = 0; leftPivot < barsLeft; leftPivot++)
+				{
+					if (subSet[leftPivot].Low < valueToCheck.Low)
+					{
+						isPivot = false;
+						break;
+					}
+				}
+
+                // If it's still a pivot by this point, carry on checking the right side...
+				if (isPivot)
+				{
+					// If the [barsRight] right side all have lower highs, it's a pivot!
+					for (int rightPivot = barsLeft + 1; rightPivot < subSet.Count; rightPivot++)
+					{
+						if (subSet[rightPivot].Low <= valueToCheck.Low)
+						{
+							isPivot = false;
+							break;
+						}
+					}
+
+                    // If it's a pivot
+					if (isPivot)
+						result.Add(valueToCheck.Low);
+					else
+						result.Add(null);
+				}
+				else
+				{
+					result.Add(null);
+				}
+			}
+
+			return result;
+		}
+	}
+}
